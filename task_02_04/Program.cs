421c377f9800b1965164a475b61b007c0117c9ec
milana-1111 <milkyway_1_d@mail.ru>
2025-01-18 09:11:53 +0500

    Выполнена 2 и 4 задача 2 урока

---
+++ task_02_04/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace task_02_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ведите нынешний год");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите нынешний месяц");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите нынешний день");
+            int z = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите свой год рождения: \n ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите свой месяц рождения: \n");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите дату рождения: \n");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            int d = x - a;
+             if ((y < b & d== 18) || (y==b & z<c))
+            { d -= 1; }
+            Console.WriteLine(d);
+            if (d >= 18)
+            {
+                Console.WriteLine("Совершеннолетний");
+
+            }
+            else
+            {
+                Console.WriteLine("Несовершеннолетний");
+            }
+
+
+
+
+
+
+            }
+    }
+}
