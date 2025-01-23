87f45abf0158b0e7b8d367d12d5c4959434c4a95
milana-1111 <milkyway_1_d@mail.ru>
2025-01-23 14:16:54 +0500

    задачи с 3 урока

---
+++ Task_03_05/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task_03_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+//            Написать программу, которая выводит на экран таблицу соответствия температуры в градусах Цельсия и
+//Фаренгейта(F = C * 1, 8 + 32).Диапазон изменения температуры в градусах Цельсия и шаг должны вводиться во
+//время работы программы
+            // Ввод начальной температуры в Цельсия
+            Console.WriteLine("Введите начальную температуру в градусах Цельсия: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            // Ввод конечной температуры в Цельсия
+            Console.WriteLine("Введите конечную температуру в градусах Цельсия: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            // Ввод шага
+            Console.WriteLine("Введите шаг изменения температуры в градусах Цельсия:");
+            double z = Convert.ToDouble(Console.ReadLine());
+
+            // Вывод таблицы 
+            Console.WriteLine("\nЦельсий  tФаренгейт");
+            Console.WriteLine(" ");
+
+            //Цикл
+            for (double C = x; C <= y; C += z)
+
+            {
+                double F = C * 1.8 + 32;
+                Console.WriteLine($"{C:F2}\t{F:F2}");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ Task_03_07/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_03_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Написать программу, которая выводит таблицу скорости(через каждые 0, 5с) свободно падающего тела v = g t ,
+            //где 2 g = 9,8 м / с2 – ускорение свободного падения.
+
+            const double g = 9.8;
+            double p = Convert.ToDouble(Console.ReadLine());
+            for (double i = 0.5; i <= p; i = i + 0.5)
+            {
+                double v = g * i;
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
