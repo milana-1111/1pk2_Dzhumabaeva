168d75f329a667a1dbdfcc0612c104a029e4acec
milana-1111 <milkyway_1_d@mail.ru>
2025-01-26 18:11:30 +0500

    задача 4 урока

--- Task_04_06/Program.cs
+++ Task_04_06/Program.cs
@@ -8,38 +8,19 @@
             //модулю были разными.Это значит, что в массиве не может быть ни только двух равных чисел, но не может быть
             //двух равных по модулю.В полученном массиве найти наибольшее по модулю число.
 
-            int mannym = 10; //создаем массив
-            int[] namb = new int[mannym];
-            HashSet<int> seTTS = new HashSet<int>();
-            Random randdd = new Random();
-            for (int i = 0; i < mannym; i++)
+            int[] x = new int[10];
+            Random rnd = new Random();
+            Console.WriteLine("Вывод чисел в массиве: ");
+            for (int i = 0; i < x.Length; i++)
+
             {
-                int namber ;
-                do
-                {
-                    //Генерируем рандомное число от -mannym до mannym
-                    namber = randdd.Next(-mannym, mannym + 1);
-                }
-                while (seTTS.Contains(Math.Abs(namber)) || namber == 0);
-                namb [i] = namber;
-                seTTS.Add(Math.Abs(namber));
+                x[i] = rnd.Next(-100, 100);
+
+                Console.WriteLine(x[i]);
             }
-            //Выводим массив
-            Console.WriteLine("Сгенерированный массив:");
-            foreach (var num in namb)
-            {
-                Console.WriteLine(num);
-            }
-            //Находим наибольшее по модулю число
-            int max1 = namb[0];
-            for (int i = 1; i < namb.Length; i++)
-            {
-                if (Math.Abs(namb[i]) > Math.Abs(max1))
-                {
-                    max1 = namb[i];
-                }
-            }
-            Console.WriteLine($"Наибольшее по модулю число:{max1}");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Максимальное число по модулю : ");
+            Console.WriteLine(x.Max(Math.Abs));
         }
 
             
