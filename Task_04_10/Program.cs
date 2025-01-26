fd8639f76e8a1e9dec27959e4c76d9eb0aed4045
milana-1111 <milkyway_1_d@mail.ru>
2025-01-26 17:57:28 +0500

    задачи 4 урока

--- Task_04_10/Program.cs
+++ Task_04_10/Program.cs
@@ -4,28 +4,27 @@
     {
         static void Main(string[] args)
         {
-//            Заполнить массив из 10 элементов случайными числами в интервале[-10..10] и сделать реверс элементов
-//отдельно для 1 - ой и 2 - ой половин массива. Реверс реализовать через цикл. Стандартные методы класса Array
-//использовать нельзя.
+            //            Заполнить массив из 10 элементов случайными числами в интервале[-10..10] и сделать реверс элементов
+            //отдельно для 1 - ой и 2 - ой половин массива. Реверс реализовать через цикл. Стандартные методы класса Array
+            //использовать нельзя.
 
-            Random rraruy = new Random();
-            int[] amary = new int[10];
-
-            //заполняем массив случайными числами
-
-            for (int i = 0; i < amary.Length; i++)
+            int[] mil = new int[10];
+            Random rnd = new Random();
+            Console.WriteLine("исходный массив : ");
+            for (int i = 0; i < mil.Length; i++)
             {
-                amary[i] = rraruy.Next(-10, 11); /*генерация числа от -10 до 10*/
+                mil[i] = rnd.Next(-10, 11);
+                Console.WriteLine(mil[i]);
             }
-            Console.WriteLine("Исходный массив:");
-            Priamary(amary);
-
-            //Реверс 1ой половины массива
-
-
-
+            Console.WriteLine("Результат:");
+            int x = mil.Length / 2;
+            for (int i = x; i >= 0; i--)
             {
-                
+                Console.WriteLine(mil[i]);
+            }
+            for (int j = 9; j >= 5; j--)
+            {
+                Console.WriteLine(mil[j]);
             }
         }
     }
