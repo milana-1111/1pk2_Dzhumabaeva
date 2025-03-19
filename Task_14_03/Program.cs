fadf04129e7f21d1259f43b74e9af87ca27c3685
milana-1111 <milkyway_1_d@mail.ru>
2025-03-20 01:12:28 +0500

    задачи 14 урока

---
+++ Task_14_03/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task_14_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Реализуйте статический метод Factorial, который принимает целое число и возвращает его факториал.
+            //Сделайтетак, чтобы метод работал только для неотрицательных чисел.
+
+            Console.WriteLine("Введите неотрицательное целое число:");
+            if (long.TryParse(Console.ReadLine(), out long n) && n >= 0)
+            {
+                Console.WriteLine($"Факториал числа {n} = {Factorial(n)}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода. Факториал определен только для неотрицательных чисел.");
+            }
+        }
+        /// <summary>
+        /// Вычисление факториала числа.
+        /// </summary>
+        /// <param name="x>Число, для которого считается факториал.</param>
+        /// <returns>Факториал числа.</returns>
+        static long Factorial(long x)
+        {
+            long factorial = 1;
+            for (long i = 2; i <=x; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
---
+++ Task_14_04/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_14_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Определите класс User, который будет иметь статическое свойство CurrentUser,
+            //представляющеетекущегопользователя, и метод для его установки.
+            User.SetCurrentUser("Милана");
+            Console.WriteLine($"Текущий пользователь: {User.CurrentUser}");
+            User.SetCurrentUser("Лера");
+            Console.WriteLine($"Текущий пользователь: {User.CurrentUser}");
+        }
+    }
+}
+public class User
+{
+    // Статическое свойство для хранения текущего пользователя
+    public static string CurrentUser { get; private set; }
+    // Метод
+    public static void SetCurrentUser(string username)
+    {
+        CurrentUser = username;
+    }
+}
