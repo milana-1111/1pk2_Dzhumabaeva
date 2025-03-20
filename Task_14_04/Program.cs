f206d87250c0f050f057ccc8d069cc5432851396
milana-1111 <milkyway_1_d@mail.ru>
2025-03-20 16:44:52 +0500

    задача 14 урока

--- Task_14_04/Program.cs
+++ Task_14_04/Program.cs
@@ -13,13 +13,3 @@
         }
     }
 }
-public class User
-{
-    // Статическое свойство для хранения текущего пользователя
-    public static string CurrentUser { get; private set; }
-    // Метод
-    public static void SetCurrentUser(string username)
-    {
-        CurrentUser = username;
-    }
-}
---
+++ Task_14_04/User.cs
@@ -0,0 +1,10 @@
+﻿public class User
+{
+    // Статическое свойство для хранения текущего пользователя
+    public static string CurrentUser { get; private set; }
+    // Метод
+    public static void SetCurrentUser(string username)
+    {
+        CurrentUser = username;
+    }
+}
