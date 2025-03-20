3f33dc0e6acd4979c8317b8650da2853cbcd891b
milana-1111 <milkyway_1_d@mail.ru>
2025-03-20 16:43:38 +0500

    задачи 13 урока

---
+++ Task_13_02/Pet.cs
@@ -0,0 +1,61 @@
+﻿namespace Task_13_02
+{
+    internal class Pet
+    {
+        static void Main(string[] args)
+        {
+            //создать класс питомца
+            //свойства:
+            //кличка, вид животного, возраст, вес, отметка о состонии здоровья(здоров / нездоров)
+            //методы:
+            //вывод информации об объекте
+            //изменение значения веса животного
+            //изменение отметки о состоянии здоровья
+            //конструторы:
+            // предусмотрите разные варианты инициализации объектов
+        }
+            // Свойства
+            public string Nickname { get; set; }
+            public string AnimalType { get; set; }
+            public int Age { get; set; }
+            public double Weight { get; set; }
+            public string HealthStatus { get; set; }
+            // Конструктор
+            public Pet()
+            {
+                Nickname = "Максик";
+                AnimalType = "Кот";
+                Age = 2;
+                Weight = 3;
+                HealthStatus = "Здоров";
+            }
+            // Конструктор с параметрами
+            public Pet(string nickname, string animalType, int age, double weight, string healthStatus)
+            {
+                Nickname = nickname;
+                AnimalType = animalType;
+                Age = age;
+                Weight = weight;
+                HealthStatus = healthStatus;
+            }
+            // Методы
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"Кличка: {Nickname}");
+                Console.WriteLine($"Вид животного: {AnimalType}");
+                Console.WriteLine($"Возраст: {Age} лет");
+                Console.WriteLine($"Вес: {Weight} кг");
+                Console.WriteLine($"Состояние здоровья: {HealthStatus}");
+            }
+            public void ChangeWeight(double newWeight)
+            {
+                Weight = newWeight;
+                Console.WriteLine($"Вес питомца изменён на: {Weight} кг");
+            }
+            public void ChangeHealthStatus(string newStatus)
+            {
+                HealthStatus = newStatus;
+                Console.WriteLine($"Состояние здоровья изменено на: {HealthStatus}");
+            }
+        }
+    }
