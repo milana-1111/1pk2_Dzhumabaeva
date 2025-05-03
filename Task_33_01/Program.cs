fa4ce76b8df9e832696d5b0ebee29c537bc815f3
milana-1111 <milkyway_1_d@mail.ru>
2025-05-03 11:34:23 +0500

    задача 33 урока

---
+++ Task_33_01/Program.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleApp3;
+using System.Drawing;
+
+namespace Task_33_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<IDrawable> list = new List<IDrawable>
+                {
+                       new Circle(5),
+                       new Square(2,7),
+                       new Circle(10),
+                       new Rectangle(7),
+                       new Circle(2),
+                       new Rectangle(10)
+
+                };
+
+
+                foreach (var item in list)
+                    item.Draw();
+
+                list.Add(new Circle(0));
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Task_33_01/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp3;
+internal class Rectangle : IDrawable
+{
+    private int height;
+    public int Height
+    {
+        get => height;
+        set
+        {
+            if (value < 1)
+                throw new ArgumentException("высота не должна быть меньше или равна 1");
+            else
+                height = value;
+        }
+    }
+    public Rectangle(int height)
+    {
+        Height = height;
+    }
+
+    public void Draw()
+    {
+        Console.WriteLine();
+        for (int i = 0; i <= height; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
